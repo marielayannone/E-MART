6fb99acf291e1dc9b2520d82cf95591db8d49a8a
marielayannone <122804646+marielayannone@users.noreply.github.com>
2025-05-24 11:08:34 -0300

    Add files via upload

---
+++ e-mart-inicio/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-orange-500">Bienvenidos a E-Mart</h1>
+      <p className="mt-4 text-lg text-gray-700">Compra y vende a tu manera</p>
+    </main>
+  );
+}
